99cd9fbc88feb73f5d0c782171c57c92da52e4d5
Giulia Gonçalves <91676330+giuliacgoncalves@users.noreply.github.com>
2023-05-19 21:13:16 -0400

    Adicionando Continuidade
    Inserindo lógica para identificar em que cor vai cair, para inserir a pergunta que será aleatória.

--- JS/wheel.js
+++ JS/wheel.js
@@ -1,4 +1,5 @@
-(function(){
+function rotateWheel(){
+    console.log('oi')
     const wheel = document.querySelector(".wheel");
     const markerBtn = document.querySelector(".marker");
     let deg = 0;
@@ -6,7 +7,7 @@
     markerBtn.addEventListener('click', () => {
         markerBtn.style.pointerEvents = 'none';
         deg = Math.floor(5000 + Math.random()*3600);
-        wheel.style.transition = 'all 10s ease-out';
+        wheel.style.transition = 'all 10s ease-in-out';
         wheel.style.transform = `rotate(${deg}deg)`;
     })
 
@@ -17,4 +18,38 @@
         wheel.style.transform = `rotate(${actualDeg}deg)`;
     })
 
-})();
+    verificarResultado(deg)
+};
+
+
+function verificarResultado(deg) {
+    if(deg >=9 && deg <=58) {
+        showPurpleQuestion()
+    }else if(deg >=69 && deg <=117){
+        showPinkQuestion()
+    }else if(deg >=128 && deg <=177){
+        showRedQuestion()
+    }else if(deg >=188 && deg <=237){
+        showYellowQuestion()
+    }else if(deg >=248 && deg <=297){
+        showGreenQuestion()
+    }else if(deg >=309 && deg <=357){
+        showGreenQuestion()
+    }
+}
+
+// Função para enviar as perguntas do tema roxo
+// mostrarPerguntaRoxa(){
+
+    // Gerar um numero aleatorio entre 0 e o tamnho da minha lista
+    // Para colocar na função do calculo
+    // perguntas.roxa.length
+
+// }
+
+// Objeto com Lista
+let perguntas = {
+    roxo: [
+        'askd'
+    ]
+}
