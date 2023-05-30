7b18f5ec9e5d3aa7f47b07aeb6887ceb4ed0dc6c
Giulia Gonçalves <91676330+giuliacgoncalves@users.noreply.github.com>
2023-05-30 19:37:15 -0400

    Correção Perguntas e Respostas

--- JS/wheel.js
+++ JS/wheel.js
@@ -35,7 +35,6 @@ wheel.addEventListener("transitionend", () => {
 });
 
 function verificarResultado(deg) {
-  console.log(deg);
   if (deg >= 0 && deg <= 60) {
     showQuestion("purple");
   } else if (deg >= 61 && deg <= 120) {
@@ -78,24 +77,28 @@ function showQuestion(color) {
 
   if(listQuestions!=undefined || listAns!=undefined){
     showAns(listAns)
-    noRepeatQuestion(listAns,listQuestions,randomQuestion)
-
+    noRepeatQuestion(listAnswer,listQuestions,randomQuestion)
   }
 }
 
 function showAns(lista) {
+  var flag = true
   var list = lista
   var lenAns = list.length;
   btnAns.forEach((btn) => {
     var randomAns = Math.floor(Math.random() * lenAns);
     var itemList = list[randomAns];
-    console.log(randomAns);
     btn.innerHTML = itemList;
+
+    if(randomAns == 0 && flag) {
+      btn.classList.add("rightAnswer")
+      flag = false
+    }
+
     list = list.filter(function (item) {
       return item !== itemList;
     });
     btn.classList.add(randomAns)
-    console.log(list);
     lenAns--;
 
     //  list.splice(randomAns, 1);
@@ -108,21 +111,12 @@ btnAns.forEach((btn) => {
 
 function compareAnswer(evt){
   let alvoClicado = evt.target;
-  let valor = alvoClicado.innerHTML;
-
+  const rightAnswer = alvoClicado.classList.contains("rightAnswer")
   let parentElement = alvoClicado.parentNode;
   let siblings = Array.from(parentElement.children);
   let targetSiblings = siblings.filter(sibling => sibling !== alvoClicado);
-
-  let ansPurple = answers.purple
-  let ansPink = answers.pink
-  let ansRed = answers.red
-  let ansYellow = answers.yellow
-  let ansGreen = answers.green
-  let ansBlue = answers.blue
-
-  if(valor == ansPurple[0][0]||valor == ansPurple[1][0]||valor == ansPurple[2][0]){
-    console.log(ansPurple)
+  
+  if(rightAnswer) {
     alvoClicado.style.backgroundColor = 'lightgreen';
     points++;
     rightWrong.innerHTML = "Correto!";
@@ -130,52 +124,8 @@ function compareAnswer(evt){
     targetSiblings.forEach((sibling=>{
       sibling.disabled = true
     }))
-  }else if(valor == ansPink[0][0]||valor == ansPink[1][0]||valor == ansPink[2][0]){
-    console.log(ansPink)
-    alvoClicado.style.backgroundColor = 'lightgreen';
-    points++;
-    rightWrong.innerHTML = "Correto!";
-    rightWrong.style.visibility = 'visible'
-    targetSiblings.forEach((sibling=>{
-      sibling.disabled = true
-    }))
-  }else if(valor == ansRed[0][0]||valor == ansRed[1][0]||valor == ansRed[2][0]){
-    console.log(ansRed)
-    alvoClicado.style.backgroundColor = 'lightgreen';
-    points++;
-    rightWrong.innerHTML = "Correto!";
-    rightWrong.style.visibility = 'visible'
-    targetSiblings.forEach((sibling=>{
-      sibling.disabled = true
-    }))
-  }else if(valor == ansYellow[0][0]||valor == ansYellow[1][0]||valor == ansYellow[2][0]){
-    console.log(ansYellow)
-    alvoClicado.style.backgroundColor = 'lightgreen';
-    points++;
-    rightWrong.innerHTML = "Correto!";
-    rightWrong.style.visibility = 'visible'
-    targetSiblings.forEach((sibling=>{
-      sibling.disabled = true
-    }))
-  }else if(valor == ansGreen[0][0]||valor == ansGreen[1][0]||valor == ansGreen[2][0]){
-    console.log(ansGreen)
-    alvoClicado.style.backgroundColor = 'lightgreen';
-    points++;
-    rightWrong.innerHTML = "Correto!";
-    rightWrong.style.visibility = 'visible'
-    targetSiblings.forEach((sibling=>{
-      sibling.disabled = true
-    }))
-  }else if(valor == ansBlue[0][0]||valor == ansBlue[1][0]||valor == ansBlue[2][0]){
-    console.log(ansBlue)
-    alvoClicado.style.backgroundColor = 'lightgreen';
-    points++;
-    rightWrong.innerHTML = "Correto!";
-    rightWrong.style.visibility = 'visible'
-    targetSiblings.forEach((sibling=>{
-      sibling.disabled = true
-    }))
-  }else{
+  }
+  else {
     worngQuestion++;
     alvoClicado.style.backgroundColor = 'red';
     rightWrong.innerHTML = "Errado!";
@@ -187,6 +137,8 @@ function compareAnswer(evt){
   score.innerHTML = "Score: " + points;
   score.style.visibility = 'visible';
   markerBtn.style.pointerEvents = "auto";
+
+  parentElement.querySelector(".rightAnswer").classList.remove("rightAnswer")
 }
 
 function compareResults(right,wrong){
@@ -194,9 +146,10 @@ function compareResults(right,wrong){
   return (right/total)*100;
 }
 
-function noRepeatQuestion(listAns,listQuestions,randomNum){
+function noRepeatQuestion(listAnswer,listQuestions,randomNum){
+  
   listQuestions.splice(randomNum, 1)
-  listAns.splice(randomNum,1)
+  listAnswer.splice(randomNum,1)
 }
 
 // Objeto com Lista para as perguntas
